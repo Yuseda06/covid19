e75a74d4df050c1d42462062490336f182a0add3
Ku Muhd Yusri Ku Saad <yusryku@gmail.com>
2022-08-27 12:26:07 +0800

    Base

---
+++ src/screens/CovidScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CovidScreen = () => {
+  return (
+    <View>
+      <Text>CovidScreen</Text>
+    </View>
+  )
+}
+
+export default CovidScreen
+
+const styles = StyleSheet.create({})
